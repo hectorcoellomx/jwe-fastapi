1484740e978a1813b5b5dc6a3f81842b91b0a04e
Hector Coello <hector_m3@live.com.mx>
2025-01-19 15:27:07 -0600

    Mejoras

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 import base64
 import json
 
@@ -6,6 +7,19 @@ from jwcrypto import jwk, jwe as jwe_module
 from jwcrypto.common import json_encode
 
 app = FastAPI()
+
+origins = [
+    "http://127.0.0.1:8000",
+    "http://localhost:8000",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 def create_jwe_token(user_data):
 
@@ -34,11 +48,24 @@ async def jwe(data):
     decoded_string = base64.b64decode(data).decode('utf-8')
     result_array = decoded_string.split(',')
     
-    if(len(result_array)==1):
+    if(len(result_array)==6):
+        user_data = { 
+            "federation": { 
+                "userId" : result_array[0], 
+                "email" : result_array[1], 
+                "termsAccepted" : False 
+            },
+            "userInfo": { 
+                "person" : { "personName" : { "givenName" : result_array[2], "lastName" : result_array[3], "secondLastName" : result_array[4] } }, 
+                "userEntities" : { "userId" : result_array[0], "entity" : { "entityId" : result_array[5] } }, 
+                "userNotificationsGroups" : { "mandatory" : [ "student" ] } 
+                } 
+            }
+    elif(len(result_array)==1):
         user_data = { "federation": { "userId" : result_array[0] } }
     else:
-        user_data = { "federation": { "userId" : result_array[0] } }
+        user_data = {}
 
     token = create_jwe_token(user_data)
 
-    return { "data": token, "value" : result_array}
+    return { "data": token, "value" : user_data}
